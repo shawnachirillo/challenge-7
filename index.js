924f1ebec0d15de9a4a915df19ad0b9269574d7d
Shawna Chirillo <schirillo711@gmail.com>
2025-01-19 06:01:33 -0600

    made updates to README

--- index.js
+++ index.js
@@ -1,52 +1,83 @@
 // TODO: Include packages needed for this application
 import inquirer from "inquirer";
 import fs from "fs";
-import { type } from "os";
-import { error } from "console";
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: "input",
         name: "title",
-        message: "What is the tite of your project?",   
-    },
+      message: "What is the tite of your project?",   
+  },
+{
+    type: "list",
+    choices: [
+        {
+            name:"Apache license 2.0 ", 
+            value: "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+        },
 
+         {name:"Creative Commons license family",
+            value: "[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)"
+         },
+    ],
+    name: "license",
+    message: "Please select a license.",
+   	
+},
     {
-        type: "input",
-        name: "description",
-        message: "Provide a description of the project.",
-    },
-{
-    type: "input",
-    name: "installation",
-    message: "Please give installation information.",
-},
-{
-    type: "input",
-    name: "usage",
-    message: "Please give ways to use this project.",
-},
-{
-    type: "input",
-    name: "constributing",
-    message: "Please provide the contribution instructions.",
-},
-{
-    type: "input",
-    name: "tests",
-    message: "Please provide test instructions.",
-},
-{
-    type: "input",
-    name: "github",
-    message: "What is your GitHub username?",
-},
+      type: "input",
+      name: "description",
+      message: "Provide a description of the project.",
+         },
+    
+ {
+   type: "input",
+  name: "installation",
+     message: "Please give installation information.",
+ },
+//  {
+//     type: "list",
+//     choices: [
+//          {
+//              name:"Apache license 2.0 ", 
+//              value: "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+//          },
+
+//          // {name:"Creative Commons license family"},
+//      ],
+//      name: "license",
+//      message: "Please select a license.",
+//     // Apache license 2.0	
+//     //Creative Commons license family	
+//  }
+
+ {
+     type: "input",
+     name: "usage",
+     message: "Please give ways to use this project.",
+ },
+ {
+     type: "input",
+     name: "constributing",
+     message: "Please provide the contribution instructions.",
+ },
+ {
+     type: "input",
+     name: "tests",
+     message: "Please provide test instructions.",
+ },
+ {
+     type: "input",
+     name: "github",
+     message: "What is your GitHub username?",
+ },
 {
     type: "input",
     name: "email",
     message: "Please enter your email address.",
-},
+ },
+
 ];
 
 
@@ -62,12 +93,41 @@ function writeToFile(fileName, data) {
         }
 //README generator
 function createREADME(answers) {
-    return `
-    #${answers.title}
+    return `# ${answers.title}
 ## Description
 ${answers.description}
+
+
+## Table Of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+${answers.installation}
 ## License
-Licensed Project
+${answers.license} 
+## Usage
+${answers.usage}
+## Contributing
+${answers.contributing}
+## Tests
+${answers.tests}
+## Questions
+${answers.installation}
+## Github
+${answers.github}
+## Email
+${answers.email}
+
+
+
+
+
+
+
+
 
 ## Questions
 Any questions please reach out.
--- readme.md
+++ readme.md
@@ -1,9 +1,38 @@
-
-    #APp
+# time managment app
 ## Description
-It's an app
+user friendly app to stay on task
+
+
+## Table Of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+clone repository, start repository
 ## License
-Licensed Project
+[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0) 
+## Usage
+open the app in your browser
+## Contributing
+undefined
+## Tests
+npm test
+## Questions
+clone repository, start repository
+## Github
+shawnachirillo
+## Email
+smscott711@yahoo.com
+
+
+
+
+
+
+
+
 
 ## Questions
 Any questions please reach out.
